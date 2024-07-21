0ac6b8704367e9ec3d968c430de40429eadee9ab
Drew Glavich <glavich.drew@optimind.tech>
2024-07-21 14:50:37 +0900

    added some options to treesitter
    I'm not entirely sure what exatly these options do yet, but them seem
    like they may be convenient.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,22 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   opts = {
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
     ensure_installed = {
       "lua",
       "vim",
-      -- add more arguments for adding more treesitter parsers
+      "markdown",
+      "typescript",
+      "javascript",
     },
   },
 }
