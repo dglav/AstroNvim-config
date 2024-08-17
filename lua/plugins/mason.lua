a2e7ded005586106f27a5e7e4a2ca521e63d8f99
Drew Glavich <drewglavich@Drews-MacBook-Pro-2.local>
2024-08-17 13:00:16 +0900

    feat: added prettierd and shfmt formatters

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -21,6 +19,7 @@ return {
     opts = {
       ensure_installed = {
         "stylua",
+        "shmft",
       },
     },
   },
