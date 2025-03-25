15137a73f699608d03cf723226b42ddeae04e2a4
Drew Glavich <glavich.drew@optimind.tech>
2025-03-25 13:13:59 +0900

    chore: delete unused plugins

--- ftplugin/markdown.lua
+++
@@ -1,8 +0,0 @@
--- vim.opt.textwidth = 80
--- vim.opt_local.formatoptions:append "t"
-
--- vim.opt.wrap = true
--- vim.opt.formatoptions:append "t"
--- vim.opt.colorcolumn = "101"
--- vim.opt_local.spell = true
--- vim.opt.linebreak = true
