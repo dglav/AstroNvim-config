2cfbc2b61a962d08d79ebf7ab5770a5f6cf0cb79
Drew Glavich <glavich.drew@optimind.tech>
2025-04-15 11:47:00 +0900

    fix: don't format typescript on save

--- lua/community.lua
+++ lua/community.lua
@@ -6,7 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
   { import = "astrocommunity.lsp.lspsaga-nvim" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.typescript" }
 }
